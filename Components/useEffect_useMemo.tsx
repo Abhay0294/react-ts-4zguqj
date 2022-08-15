a35dff5dfc77144f5f72238771e82311508441f9
Abhay0294 <abhaypratap349@gmail.com>
2022-08-15 12:40:31 +0530

    2 files modified

--- Components/useEffect_useMemo.tsx
+++ Components/useEffect_useMemo.tsx
@@ -4,19 +4,28 @@ import './../style';
 function ButtonCounter() {
   const [count, setState] = React.useState(0);
 
+  const clickHandler = () => {
+    setState(count + 1);
+  };
+
   return (
     <div className="parent-container">
-      <button
-        className="button-counter"
-        onClick={() => {
-          setState(count + 1);
-        }}
-      >
-        Click +/-
+      <button className="button-counter" onClick={clickHandler}>
+        Click +
       </button>
-      <input placeholder="counter values">{this.count}</input>
+      <input
+        placeholder="counter values"
+        value={`You clicked ${count} times`}
+      ></input>
+      <button className="button-counter" onClick={clickHandler}>
+        Click -
+      </button>
     </div>
   );
 }
 
 export default ButtonCounter;
+
+/**
+ * We don't use 'this' in react's functional componenet
+ */
